c279361105a524e57c0d2634693c5cbc4500fb70
Felipe Martín <fmartingr@gmail.com>
2015-07-01 23:23:48 +0200

    Better notifications admin

--- amiibofindr/apps/notifications/admin.py
+++ amiibofindr/apps/notifications/admin.py
@@ -11,6 +11,10 @@ from .models import AmiiboNotification
 
 
 class AmiiboNotificationAdmin(reversion.VersionAdmin):
+    list_display = ('amiibo', 'max_price', 'interval', 'shops_list', 'last_notification',)
     filter_horizontal = ('shops', )
+
+    def shops_list(self, obj):
+        return ','.join(obj.shops.values_list('shop__name', flat=True))
 
 admin.site.register(AmiiboNotification, AmiiboNotificationAdmin)
