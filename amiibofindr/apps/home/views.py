5ec8112558619969213a962017fccad58d863d44
Felipe Martín <fmartingr@gmail.com>
2015-09-07 23:17:06 +0200

    Fixed home redirect reverse

--- amiibofindr/apps/home/views.py
+++ amiibofindr/apps/home/views.py
@@ -4,8 +4,9 @@
 from django.views.generic.base import View
 from django.shortcuts import redirect
 from django.core.urlresolvers import reverse
+from django.utils.translation import ugettext as _
 
 
 class HomeView(View):
     def get(self, request):
-        return redirect(reverse('amiibo:collection', args=['all']))
+        return redirect(reverse('amiibo:collection', args=[_('all')]))
