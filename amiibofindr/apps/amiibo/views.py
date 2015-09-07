5ff3bce5e2ba4ad153208698fc3aec2b612301f9
Felipe Martín <fmartingr@gmail.com>
2015-09-07 21:02:21 +0200

    Added i18n to 'all' amiibo list

--- amiibofindr/apps/amiibo/views.py
+++ amiibofindr/apps/amiibo/views.py
@@ -3,6 +3,7 @@
 # django
 from django.shortcuts import render, get_object_or_404
 from django.views.generic.base import View
+from django.utils.translation import gettext as _
 
 # amiibo
 from amiibofindr.apps.amiibo.models import Collection, Amiibo
@@ -10,9 +11,10 @@ from amiibofindr.apps.amiibo.models import Collection, Amiibo
 
 class CollectionView(View):
     template = 'amiibo/collection.html'
+    all_word = _('all')
 
     def get(self, request, collection='all'):
-        if collection != 'all':
+        if collection != self.all_word:
             collection = get_object_or_404(Collection, slug=collection)
             amiibo_list = collection.amiibos
         else:
